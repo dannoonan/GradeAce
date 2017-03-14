a6614dfde613b15c8f38dbf160a669091e9e3499
Kevin Moynihan <kevinmoynihan23@gmail.com>
2017-03-14 17:42:21 +0000

    New Sql Tables

--- createTables.sql
+++ createTables.sql
@@ -5,37 +5,71 @@ CREATE TABLE IF NOT EXISTS `Users` (
 `Email` varchar(128) NOT NULL,
 `Course` varchar(128) NOT NULL,
 `Password` varchar(128) NOT NULL,
-`Reputation` SMALLINT UNSIGNED NOT NULL,
+`Reputation` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
 PRIMARY KEY (`UserId`)
 );
 
 CREATE TABLE IF NOT EXISTS `Tasks` (
-`UserId` int(20) unsigned NOT NULL,
 `TaskId` int(10) unsigned NOT NULL AUTO_INCREMENT,
 `Title` varchar(128) NOT NULL,
 `TaskType` varchar(128) NOT NULL,
-`Description` varchar(4096) DEFAULT NULL,
-`Tags` varchar(1000) DEFAULT NULL,
+`Description` varchar(4096) NOT NULL,
 `Pages` int(5) unsigned NOT NULL,
 `Words` int(10) unsigned NOT NULL,
 `FileFormat` char(128) NOT NULL,
 `FilePath` varchar(250) NOT NULL,
 `ClaimDate` datetime NOT NULL,
 `CompleteDate` datetime NOT NULL,
-PRIMARY KEY (`TaskId`),
-UNIQUE KEY (`FilePath`),
-KEY `fk_TaskOwnerId` (`UserId`),
-CONSTRAINT `fk_TaskOwnerId` FOREIGN KEY (`UserId`) REFERENCES `Users` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-
-
-CREATE TABLE IF NOT EXISTS `CompleteTasks` (
-`TaskId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-`UserId` int(20) unsigned NOT NULL ,
-`Title` varchar(128) NOT NULL,
 `Notes` varchar(300) DEFAULT NULL,
 PRIMARY KEY (`TaskId`),
-KEY `fk_two_TaskOwnerId` (`UserId`),
-CONSTRAINT `fk_two_TaskOwnerId` FOREIGN KEY (`UserId`) REFERENCES `Users` (`UserId`) ON DELETE CASCADE ON UPDATE CASCADE
+UNIQUE KEY (`FilePath`)
 );
+
+CREATE TABLE IF NOT EXISTS `Owned` (
+`Numbered` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+`UserId` int(10) unsigned NOT NULL,
+`TaskId` int(10) unsigned NOT NULL ,
+PRIMARY KEY (`Numbered`),
+FOREIGN KEY (`UserId`) REFERENCES USERS(`UserId`),
+FOREIGN KEY (`TaskId`) REFERENCES TASKS(`TaskId`)
+);
+
+CREATE TABLE IF NOT EXISTS `Flag` (
+`Numbered` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+`TaskId` int(10) unsigned NOT NULL,
+`IsFlagged` boolean NOT NULL DEFAULT 0,
+PRIMARY KEY (`Numbered`),
+FOREIGN KEY (`TaskId`) REFERENCES TASKS(`TaskId`)
+);
+
+CREATE TABLE IF NOT EXISTS `Banned` (
+`Numbered` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+`UserId` int(10) unsigned NOT NULL,
+`IsBanned` boolean NOT NULL DEFAULT 0,
+PRIMARY KEY (`Numbered`),
+FOREIGN KEY (`UserId`) REFERENCES Users(`UserId`)
+);
+
+CREATE TABLE IF NOT EXISTS `Tags` (
+`TagId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`Tag` varchar(128) unsigned NOT NULL,
+PRIMARY KEY (`TagId`)
+);
+
+CREATE TABLE IF NOT EXISTS `TaskTags` (
+`Numbered` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+`TaskId` int(10) unsigned NOT NULL,
+`TagId` int(10) unsigned NOT NULL,
+PRIMARY KEY (`Numbered`),
+FOREIGN KEY (`TaskId`) REFERENCES TASKS(`TaskId`),
+FOREIGN KEY (`TagId`) REFERENCES TAGS(`TagId`)
+);
+
+CREATE TABLE IF NOT EXISTS `StatusTable` (
+`Numbered` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+`TaskId` int(10) unsigned NOT NULL,
+`Status` int(1) unsigned NOT NULL DEFAULT 0,
+PRIMARY KEY (`Numbered`),
+FOREIGN KEY (`TaskId`) REFERENCES TASKS(`TaskId`)
+);
+
