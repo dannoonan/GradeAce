819d0d38813c6b3d0b1b831fc43b3d85af828e4c
Kevin Moynihan <kevinmoynihan23@gmail.com>
2017-03-18 19:05:21 +0000

    File upload works and Extra sql table added
    Able to upload a file to upload.sql

--- CreateTask.php
+++ CreateTask.php
@@ -4,78 +4,108 @@
 	
 	require_once('load.php');
 	
-	
 	if (isset($_POST['create_btn'])){
-		$Title=($_POST['Title']);
-		$TaskType=($_POST['TaskType']);
-		$Description=($_POST['Description']);
-		$Pages=($_POST['Pages']);
-		$Words=($_POST['Words']);
-		$FileFormat=($_POST['FileFormat']);
-		$ClaimDate=($_POST['ClaimDate']);
-		$CompleteDate=($_POST['CompleteDate']);
-		
-		$FilePath=('f');	//For now just to have all details
-			
-		$sql = "INSERT INTO tasks(Title, TaskType, Description, Pages, Words, FileFormat, FilePath, ClaimDate, CompleteDate) VALUES('$Title', '$TaskType', '$Description', '$Pages', '$Words', '$FileFormat', '$FilePath', '$ClaimDate', '$CompleteDate')";
-		mysqli_query($db, $sql);
-				
-		$TaskId = mysqli_insert_id($db)or die(mysqli_error($db));
-		
-		for($i=0; $i<4; $i++)
-		{
-			$cont=false;
-			if($i==0 && !(empty($_POST['Tag1'])))
-			{
-				$Tag=($_POST['Tag1']);
-				$cont=true;
-			}
-			else if($i==1 && !(empty($_POST['Tag2'])))
-			{
-				$Tag=($_POST['Tag2']);
-				$cont=true;
-			}
-			else if ($i==2 && !(empty($_POST['Tag3'])))
-			{
-				$Tag=($_POST['Tag3']);
-				$cont=true;
-			}
-			else if ($i==3 && !(empty($_POST['Tag4'])))
-			{
-				$Tag=($_POST['Tag4']);
-				$cont=true;
-			}
-			
-			if($cont == TRUE)
-			{
-				$Query = "Select Tag from tags where Tag = '$Tag' ";
-				if($result = mysqli_query($db, $Query)) {
-				if ( $result===NULL || mysqli_num_rows($result) == 0 ) {					
-					mysqli_query($db, "INSERT INTO tags(Tag) VALUES('$Tag')");
-					$TagId = mysqli_insert_id($db)or die(mysqli_error($db));
-					mysqli_query($db,"INSERT INTO tasktags(TaskId, TagId) Values ('$TaskId', '$TagId')");
-				} else {
-					$GetTagId=mysqli_query($db, "SELECT TagId FROM tags WHERE Tag='$Tag'");
-					$GetTagId = mysqli_fetch_array($GetTagId);
-					$TagId=$GetTagId['TagId'];
-					mysqli_query($db,"INSERT INTO tasktags(TaskId, TagId) Values ('$TaskId', '$TagId')");
-				}
-				} else {
-					echo "Query failed ". $Query;
-				}
-			}
-		}
-		
-		mysqli_query($db,"INSERT INTO flag(TaskId, IsFlagged) Values ('$TaskId', 'false')");
-		mysqli_query($db,"INSERT INTO statustable(TaskId) Values ('$TaskId')");
-		
-		$userId = $_SESSION['id'];
-		mysqli_query($db,"INSERT INTO owned(UserId, TaskId) Values ('$userId', '$TaskId')");
-				
-		//header("location: index.php");
-	}
 	
+		if(($_FILES['pdf1']['size'] > 0))
+		{
+			$Title=($_POST['Title']);
+			$TaskType=($_POST['TaskType']);
+			$Description=($_POST['Description']);
+			$Pages=($_POST['Pages']);
+			$Words=($_POST['Words']);
+			$FileFormat=($_POST['FileFormat']);
+			$ClaimDate=($_POST['ClaimDate']);
+			$CompleteDate=($_POST['CompleteDate']);
+			
+		if(empty($Title) || empty($TaskType) || empty($Description) || empty($Pages) || empty($Words) ||
+			empty($FileFormat) || empty($ClaimDate) || empty($CompleteDate))
+			{
+				echo "Information missing";
+			}
+			else
+			{
+			
+			$fileName = $_FILES['pdf1']['name'];
+			$tmpName  = $_FILES['pdf1']['tmp_name'];
+			$fileSize = $_FILES['pdf1']['size'];
+			$fileType = $_FILES['pdf1']['type'];
 
+			$fp      = fopen($tmpName, 'r');
+			$content = fread($fp, filesize($tmpName));
+			$content = addslashes($content);
+			fclose($fp);
+
+			if(!get_magic_quotes_gpc())
+			{
+				$fileName = addslashes($fileName);
+			}
+
+			$query = "INSERT INTO upload (name, size, type, content ) VALUES ('$fileName', '$fileSize', '$fileType', '$content')";
+			
+			mysqli_query($db, $query) or die('Error, query failed'); 
+			$FilePath=mysqli_insert_id($db)or die(mysqli_error($db));
+			
+			$sql = "INSERT INTO tasks(Title, TaskType, Description, Pages, Words, FileFormat, FilePath, ClaimDate, CompleteDate) VALUES('$Title', '$TaskType', '$Description', '$Pages', '$Words', '$FileFormat', '$FilePath', '$ClaimDate', '$CompleteDate')";
+			mysqli_query($db, $sql);
+				
+			$TaskId = mysqli_insert_id($db)or die(mysqli_error($db));
+		
+			for($i=0; $i<4; $i++)
+			{
+				$cont=false;
+				if($i==0 && !(empty($_POST['Tag1'])))
+				{
+					$Tag=($_POST['Tag1']);
+					$cont=true;
+				}
+				else if($i==1 && !(empty($_POST['Tag2'])))
+				{
+					$Tag=($_POST['Tag2']);
+					$cont=true;
+				}
+				else if ($i==2 && !(empty($_POST['Tag3'])))
+				{
+					$Tag=($_POST['Tag3']);
+					$cont=true;
+				}
+				else if ($i==3 && !(empty($_POST['Tag4'])))
+				{
+					$Tag=($_POST['Tag4']);
+					$cont=true;
+				}
+			
+				if($cont == TRUE)
+				{
+					$Query = "Select Tag from tags where Tag = '$Tag' ";
+					if($result = mysqli_query($db, $Query)) {
+					if ( $result===NULL || mysqli_num_rows($result) == 0 ) {					
+						mysqli_query($db, "INSERT INTO tags(Tag) VALUES('$Tag')");
+						$TagId = mysqli_insert_id($db)or die(mysqli_error($db));
+						mysqli_query($db,"INSERT INTO tasktags(TaskId, TagId) Values ('$TaskId', '$TagId')");
+					} else {
+						$GetTagId=mysqli_query($db, "SELECT TagId FROM tags WHERE Tag='$Tag'");
+						$GetTagId = mysqli_fetch_array($GetTagId);
+						$TagId=$GetTagId['TagId'];
+						mysqli_query($db,"INSERT INTO tasktags(TaskId, TagId) Values ('$TaskId', '$TagId')");
+					}
+					} else {
+						echo "Query failed ". $Query;
+					}
+				}
+			}
+		
+			mysqli_query($db,"INSERT INTO flag(TaskId, IsFlagged) Values ('$TaskId', 'false')");
+			mysqli_query($db,"INSERT INTO statustable(TaskId) Values ('$TaskId')");
+			
+			$userId = $_SESSION['id'];
+			mysqli_query($db,"INSERT INTO owned(UserId, TaskId) Values ('$userId', '$TaskId')");
+						
+			//header("location: index.php");
+		}
+	}
+	else
+		echo "NO PDF UPLOADED";
+	}
 ?>
 
 <html>
@@ -111,11 +141,11 @@
 								<h2>Create a Task by filling in the details below.</h2>
 							</header>
 							
-							<form action="CreateTask.php" method="post">
+							<form action="CreateTask.php" method="post" enctype="multipart/form-data">
 							
-							<input type="text" name="Title" placeholder="Please enter the task title.">
+							<input type="text" name="Title" placeholder="*Please enter the task title.">
 							<select name="TaskType">
-								<option value="">Select your task type...</option>
+								<option value="">*Select your task type...</option>
 								<option value="Thesis">MSc Thesis</option>
 								<option value="Dissertation">BSc Dissertation</option>
 								<option value="Report">Project Report</option>
@@ -123,15 +153,15 @@
 								<option value="Assignment">Assignment</option>
 								<option value="CRP">Conference Research Paper</option>
 							</select>
-							<input type="text" name="Description" placeholder="Please enter the task description.">
+							<input type="text" name="Description" placeholder="*Please enter the task description.">
 							<input type="text" name="Tag1" placeholder="(Optional)Please enter a tag.">
 							<input type="text" name="Tag2" placeholder="(Optional)Please enter a tag.">
 							<input type="text" name="Tag3" placeholder="(Optional)Please enter a tag.">
 							<input type="text" name="Tag4" placeholder="(Optional)Please enter a tag.">
-							Please enter the page length of the task:	<input type="number" name="Pages">
-							</br>Please enter the word count of the task:	<input type="number" name="Words">							
+							*Please enter the page length of the task:	<input type="number" name="Pages">
+							</br>*Please enter the word count of the task:	<input type="number" name="Words">							
 							<select name="FileFormat">
-								<option value="">Select your file format...</option>
+								<option value="">*Select your file format...</option>
 								<option value="pdf">PDF</option>
 								<option value="docx">Docx</option>
 								<option value="doc">Doc</option>
@@ -139,17 +169,14 @@
 								<option value="txt">Txt</option>
 								<option value="odt">Odt</option>
 							</select>
-							Please choose 3 pdf pages at random from your task to upload
+							*Please choose 3 pages at random from your task you wish to show and upload as a pdf
 							</br><input type="file" name="pdf1" />
-							</br><input type="file" name="pdf2" />
-							</br><input type="file" name="pdf3" />
 							
-							</br>Please enter the date by which the task must be claimed:	<input type="date" name="ClaimDate">
-							</br>Please enter the date by which the task must be completed:		<input type="date" name="CompleteDate">
+							</br>*Please enter the date by which the task must be claimed:	<input type="date" name="ClaimDate">
+							</br>*Please enter the date by which the task must be completed:		<input type="date" name="CompleteDate">
+							</br></br>(Note all fields marked with * need to be filled)</br>
 							<input type="submit" value="Create Task" name="create_btn">
 							</form>
-
-
 						</div>
 					</div>
 				</article>
