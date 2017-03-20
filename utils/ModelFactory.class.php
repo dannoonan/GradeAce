0875ce6755a649038d732013943a0b86d0694fdb
dannoonan <danjnoonan@hotmail.com>
2017-03-20 18:19:25 +0000

    Added ModelFactory and MySQLiAccess classes

---
+++ utils/ModelFactory.class.php
@@ -0,0 +1,131 @@
+<?php
+require_once __DIR__."//../models/Item.class.php";
+require_once __DIR__."//../models/User.class.php";
+require_once __DIR__."//../models/Tag.class.php";
+
+class ModelFactory {
+	//The public function is the interaction point between the other classes and the ModelFactory class, it provides an interface between
+	//those classes and the private classes that we want to remain encapsulated
+    public static function buildModel($modelName, $modelData) {
+
+        $retVal = null;
+		//Depending on the string passed as an arg to the buildModel funtion, either the createUser or createTask function will be called
+        switch($modelName) {
+             case "User":
+                $retVal = self::createUser($modelData);
+                break;
+			case "Tag":
+               $retVal = self::createTag($modelData);
+                break;
+			case "Task":
+                $retVal = self::createTask($modelData);
+                break;
+			default:
+                echo "Unable to build model $modelName";
+		}
+		
+		return $retVal;
+    }
+
+	private static function createUser($modelData) {
+		//User object is created
+		$retVal = new User();
+		//If the UserId field is not set, then the value is set to that of the same field in the modelData
+		if (isset($modelData['UserId'])) {
+			$retVal ->setUserId($modelData["UserId"]);
+		}
+		//The same goes for the FirstName field, and all subsequent fields 
+		if (isset($modelData['FirstName'])) {
+			$retVal ->setFirstName($modelData["FirstName"]);
+		}
+		
+		if (isset($modelData['LastName'])) {
+			$retVal ->setLastName($modelData["LastName"]);
+		}	
+
+		if (isset($modelData['Email'])) {
+			$retVal ->setEmail($modelData["Email"]);
+		}
+		
+		if (isset($modelData['Course'])) {
+			$retVal ->setCourse($modelData["Course"]);
+		}
+		
+		if (isset($modelData['Password'])) {
+			$retVal ->setPassword($modelData["Password"]);
+		}
+		if (isset($modelData['Reputation'])) {
+			$retVal ->setPassword($modelData["Reputation"]);
+		}
+		//The new User object is returned
+		return $retVal;
+	}
+	
+	private static function createTag($modelData) {
+		//User object is created
+		$retVal = new Tag();
+		//If the TagId field is not set, then the value is set to that of the same field in the modelData
+		if (isset($modelData['TagId'])) {
+			$retVal ->setUserId($modelData["TagId"]);
+		}
+		
+		if (isset($modelData['Tag'])) {
+			$retVal ->setFirstName($modelData["Tag"]);
+		}
+		
+		//The new Tag object is returned
+		return $retVal;
+	}
+	
+	private static function createTask($modelData) {
+		$retVal = new Task();
+		
+		if (isset($modelData['TaskId'])) {
+			$retVal ->setTaskId($modelData["TaskId"]);
+		}
+
+		if (isset($modelData['Title'])) {
+			$retVal ->setTitle($modelData["Title"]);
+		}
+		
+		if (isset($modelData['TaskType'])) {
+			$retVal ->setTaskType($modelData["TaskType"]);
+		}
+		
+		if (isset($modelData['Description'])) {
+			$retVal ->setDescription($modelData["Description"]);
+		}	
+
+		if (isset($modelData['Pages'])) {
+			$retVal ->setPages($modelData["Pages"]);
+		}
+
+		if (isset($modelData['Words'])) {
+			$retVal ->setWords($modelData["Words"]);
+		}
+		
+		if (isset($modelData['FileFormat'])) {
+			$retVal ->setFileFormat($modelData["FileFormat"]);
+		}
+		
+		if (isset($modelData['FilePath'])) {
+			$retVal ->setFilePath($modelData["FilePath"]);
+		}
+		
+		if (isset($modelData['ClaimDate'])) {
+			$retVal ->setClaimDate($modelData["ClaimDate"]);
+		}
+
+		if (isset($modelData['CompleteDate'])) {
+			$retVal ->setCompleteDate($modelData["CompleteDate"]);
+		}	
+
+		if (isset($modelData['Notes'])) {
+			$retVal ->setNotes($modelData["Notes"]);
+		}
+			
+		return $retVal;
+	}
+
+}
+?>
---
+++ utils/MySQLiAccess.class.php
@@ -0,0 +1,74 @@
+<?php 
+class MySQLiAccess {
+	
+	private $error_msg     = '';
+	private $connection;
+	private static $instance = null;
+	
+	
+	private function __construct() {
+		$this->openConnection();
+	}
+	
+	public static function getInstance() {
+        if (!is_null(MySQLiAccess::$instance)) {
+            return MySQLiAccess::$instance;
+        } else {
+            MySQLiAccess::$instance = new MySQLiAccess();
+            return MySQLiAccess::$instance;
+        }
+    }
+	
+    private function openConnection() {
+		$conn = false;
+		$ret = false;
+        $dbName = "gradeace";	//Settings::get('database.database');
+        $server = "localhost";  //Settings::get('database.server');
+		//Username can be changed accordingly
+        $username = "Dan";	    //Settings::get('database.username');
+        $password = "password";	        //Settingstings::get('database.password');
+		$conn = new mysqli($server, $username, $password, $dbName);
+		
+		unset($password); unset($dbName); unset($server);  unset($username);
+		
+		if (!$conn) {
+            $this->error_msg = "\r\n" . "Unable to connect to database - " . date('H:i:s');
+            $ret = false;
+        } else {
+            $this->connection = $conn;
+            $ret = true;
+        }
+        return $ret;
+	}
+	
+	public static function call($procedure, $procArgs) {
+        $db = MySQLiAccess::getInstance();
+        
+        if (!is_array($procArgs)) {
+            $sql = "CALL $procedure ($procArgs)";
+        } else {
+            $sql = "CALL $procedure (".implode(', ', $procArgs).")";
+        }
+        
+        if ((empty($sql)) || (empty($db->connection))) {
+            $db->error_msg = "\r\n" . "SQL Statement or connect is <code>null</code>" . date('H:i:s');
+            return false;
+        }
+        
+        $conn = $db->connection;
+        $data = array();
+        if ($result = $conn->query($sql)) {
+            foreach ($result as $row) {
+                $data[] = $row;
+            }
+        }
+        return empty($data) ? false : $data;
+    }
+	
+	public static function prepareString($string) {
+		$db = MySQLiAccess::getInstance();
+		$conn = $db->connection;
+		return $conn->real_escape_string($string);
+	}
+} 
+?>
